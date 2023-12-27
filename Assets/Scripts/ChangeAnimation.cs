2c0ea0db82755e02c86a1af66c5acad84c3fee63
unknown <alu0101392561@ull.edu.es>
2023-12-27 10:46:47 +0000

    Modifications on ChangeAnimation script

--- Assets/Scripts/ChangeAnimation.cs
+++ Assets/Scripts/ChangeAnimation.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class ChangeAnimation : MonoBehaviour
 {
-    public string newAnimationName;  // Asigna la nueva animación desde el Inspector
+    public string new_animation_name;
+    public float time_to_dissapear;
 
     void Start()
     {
-        /*
-        if (newAnimation != null) {
-            ChangePrefabAnimation();
-        }
-        */
         Invoke("ChangePrefabAnimation", 3.0f);
     }
 
     void ChangePrefabAnimation() {
-        Animator animatorComponent = GetComponent<Animator>();
+        Animator animator_component = GetComponent<Animator>();
 
-        if (animatorComponent == null)
-        {
+        if (animator_component == null) {
             Debug.LogError("El prefab no tiene un componente Animator.");
             return;
         }
+        animator_component.Play(new_animation_name, -1, 0f);
+        if (new_animation_name == "Death") {
+            Invoke("DestroyObject", time_to_dissapear);
+        }
+    }
 
-        // Cambia la animación utilizando el nombre de la capa de animación
-        animatorComponent.Play(newAnimationName, -1, 0f);
+    void DestroyObject() {
+        Destroy(gameObject);
     }
 }
